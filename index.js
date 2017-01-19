e1c3cbc2feb57fb5720bc4cbb35fa786be3b0320
Silvia <silviachen917@gmail.com>
2017-01-19 16:54:58 -0500

    mongo

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const path = require('path')
 const express = require('express')
-
-const port = 9888
-const app = express()
-
-app.get('/', (req, res) => {
-  res.send('MEOW')
+const { MongoClient } = require('mongodb')
+const mongoUrl = 'mongodb://localhost:27017/tutorial-app'
+MongoClient.connect(mongoUrl, (err, db) => {
+  if (err) return console.log('Failed to connect to MongoDB', err)
+  const app = express()
+  app.use(express.static('public'))
+  app.listen(3000, () => console.log('Server running on port 3000'))
 })
-
-app.listen(port, () => console.log(`Listening on port ${port}`))
